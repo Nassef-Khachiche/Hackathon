d591bf3d635042abf919a9f481df65762853abcb
JopFrenken <87734@roc-teraa.nl>
2022-03-23 14:05:37 +0100

    quiz.php + added css

--- quiz.php
+++ quiz.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/quiz-style.css">
     <title>Quiz</title>
 </head>
+
 <body>
     <?php require 'inc/nav.php'; ?>
-    <h1 class="">Quiz Pagina</h1>
-    <h2> Hoi daar! </h2>
-    <p class="h1-quiz">Wij hebben een quiz gemaakt om erachter te komen wat je dagelijks doet.Nadat je deze quiz hebt gemaakt zien jij of je ouders hulp nodig hebben.</p>  
-    <p class="naam-start">Voordat je start willen wij je naam weten.</p>
-    <p class="vul-naam">Vul je naam in!<p>
-    <input type="text" name="" id="">
-    <button class="start-quiz-btn">Start quiz</button>
+    <div class="quiz-container">
+        <h1 class="quiz-title">Quiz Pagina</h1>
+        <h2> Hoi daar! </h2>
+        <p class="p-quiz">Wij hebben een quiz gemaakt om erachter te komen wat je dagelijks doet.Nadat je deze quiz hebt
+            gemaakt zien jij of je ouders hulp nodig hebben.</p>
+        <p class="naam-start">Voordat je start willen wij je naam weten.</p>
+        <div class="input-and-btn">
+        <label for="input-name">Vul je naam in!</label>
+          <input type="text" name="input-name" class="name-input">
+          
+          <button class="start-quiz-btn">Start quiz</button>
+       </div>
+    </div>
 </body>
-</html> 
+
+</html>
