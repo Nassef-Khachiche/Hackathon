dae564f2acfcff5dd078114494c2f6268f4c3ebb
JopFrenken <87734@roc-teraa.nl>
2022-03-23 13:27:55 +0100

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -11,8 +11,10 @@
     <?php require 'inc/nav.php'; ?>
     <h1 class="">Quiz Pagina</h1>
     <h2> Hoi daar! </h2>
-    <p>Wij hebben een quiz gemaakt om erachter te komen wat je dagelijks doet.Nadat je deze quiz hebt gemaakt zien jij of je ouders hulp nodig hebben.</p>  
-    <p>Voordat je start willen wij je naam weten.</p>
-    <p>Vul je naam in!<p>
+    <p class="h1-quiz">Wij hebben een quiz gemaakt om erachter te komen wat je dagelijks doet.Nadat je deze quiz hebt gemaakt zien jij of je ouders hulp nodig hebben.</p>  
+    <p class="naam-start">Voordat je start willen wij je naam weten.</p>
+    <p class="vul-naam">Vul je naam in!<p>
+    <input type="text" name="" id="">
+    <button class="start-quiz-btn">Start quiz</button>
 </body>
 </html> 
