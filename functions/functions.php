319273665f8b9f760ce9e00ff1bbf6e3293e9db9
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-23 10:02:55 +0100

    Complete final template
    title

--- functions/functions.php
+++ functions/functions.php
@@ -1,27 +1,6 @@
 <?php
 
-function route() {
-    $request = $_SERVER['REQUEST_URI'];
-
-    switch ($request) {
-        case '/' :
-            require __DIR__ . '/index.php';
-            break;
-        case '' :
-            require __DIR__ . '/index.php';
-            break;
-        case '/about' :
-            require __DIR__ . '/views/about.php';
-            break;
-        case '/about' :
-            require __DIR__ . '/views/quiz.php';
-            break;
-        default:
-            http_response_code(404);
-            require __DIR__ . '/views/404.php';
-            break;
-    }
+function db() {
+    # Function voor db connection
 }
-
-
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="https://leergeldzwf.nl/wp-content/uploads/2020/03/leergeldZWF-icon-only-304x130-1.png">
     <link rel="stylesheet" href="css/style.css">
     <title>Home</title>
 </head>
