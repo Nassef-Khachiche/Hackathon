7f3e75121df964a9a8d3f2466d81e76b2139d5a5
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-23 09:50:35 +0100

    New Template Origin
    title

--- views/about.php
+++ views/about.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="https://leergeldzwf.nl/wp-content/uploads/2020/03/leergeldZWF-icon-only-304x130-1.png">
     <link rel="stylesheet" href="css/style.css">
-    <title>Home</title>
+    <title>about</title>
 </head>
 <body>
     
