2ffbdd86c4299030b3d503fd356d7fc0cbb5e1ae
hassan-emh <84015@roc-teraa.nl>
2022-03-23 13:02:07 +0100

    created css

---
+++ inc/nav.php
@@ -0,0 +1,10 @@
+<header class="main-nav">
+    <div class="nav-logo">
+        <img src="assets/logo.svg" alt="Logo Foto van Leergeld">
+    </div>
+    <nav class="nav-link">
+        <a href="">Home</a>
+        <a href="">Start Quiz</a>
+        <a href="">Over Ons</a>
+    </nav>
+</header>
