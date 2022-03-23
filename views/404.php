886ca93c6c72b70616e133f3011ed36294df6f23
hassan-emh <84015@roc-teraa.nl>
2022-03-23 13:26:49 +0100

    Update 404.php

--- views/404.php
+++ views/404.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <title>404</title>
+    <title>404 Error</title>
 </head>
 <body>
     
