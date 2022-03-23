14d2b169fd22f1e57ba25a12ec49a3342f71a7af
JopFrenken <87734@roc-teraa.nl>
2022-03-23 13:12:35 +0100

    added text to quiz

---
+++ quiz.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Quiz</title>
+</head>
+<body>
+    <?php require 'inc/nav.php'; ?>
+    <h1 class="">Quiz Pagina</h1>
+    <h2> Hoi daar! </h2>
+    <p>Wij hebben een quiz gemaakt om erachter te komen wat je dagelijks doet.Nadat je deze quiz hebt gemaakt zien jij of je ouders hulp nodig hebben.</p>  
+    <p>Voordat je start willen wij je naam weten.</p>
+</body>
+</html>
