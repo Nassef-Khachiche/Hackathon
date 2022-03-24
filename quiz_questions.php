184290eb393cc7b9a425a11e1cc50fc038037439
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 10:18:23 +0100

    test

--- quiz_questions.php
+++ quiz_questions.php
@@ -13,21 +13,12 @@
 
 <body>
     <?php require 'inc/nav.php'; ?>
-    <?php include 'function/functions.php'; ?>
     <div class="container-question">
         <h1 class="question-count"> Vraag 1/12 </h1>
         <?php get_question(0); ?>
         <h2 class="dummy">Testvraag</h2>
         <form action="quiz_questions.php" method="post">
             <input type="number" name="answer" id="answer" class="answer">
-            <input type="submit" name="answer" value="Volgende vraag">
-    <?php include 'functions/functions.php'; ?>
-    <div class="container-question">
-        <h1 class="question-count"> Vraag 1/12 </h1>
-        <h2 class="dummy"><?php get_question(0) ?></h2>
-        <form action="quiz_questions.php" method="post">
-            <input type="number" name="answer" id="answer" class="answer">
-            <input type="button" value="Volgende vraag" name="answer" class="btnSubmitQuestion">
             <input type="button" value="Volgende vraag" class="btnSubmitQuestion">
         </form>
     </div>
