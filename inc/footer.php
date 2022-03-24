5fd0775e6ef1c616cdca079519558f54d1dc1a54
hassan-emh <84015@roc-teraa.nl>
2022-03-24 10:19:50 +0100

    updated css and html

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,17 @@
-
+<footer class="main-footer">
+    <article class="left-side-footer">
+    Vereniging van Stichtingen Leergeld in Nederland 
+    Leergeld Nederland
+    Kooikersweg 2
+    5223 KA  Den Bosch
+    </article>
+    <article class="middle-side-footer">
+    Telefonisch bereikbaar op maandag, dinsdag en donderdag van:
+    9.00 – 12.30 en 13.30 – 15.30
+    Telefoonnummer 013 -545 1656
+    </article>
+    <article class="right-side-footer">
+    E-mail: info@leergeld.nl
+    Website: www.leergeld.nl
+    </article>
+</footer>
