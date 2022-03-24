f0de655d610440df1cfcecaa76ce252228482813
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 09:58:15 +0100

    function to get database questions

--- quiz_questions.php
+++ quiz_questions.php
@@ -15,12 +15,9 @@
     <?php include 'function/functions.php'; ?>
     <div class="container-question">
         <h1 class="question-count"> Vraag 1/12 </h1>
-        <?php //require 'functions/functions.php';
-// get_question(1);
-?>
-        <h2 class="dummy"><?php get_question(1); ?></h2>
+        <h2 class="dummy"><?php get_question(0); ?></h2>
         <form action="quiz_questions.php" method="post">
-            <input type="number" name="answer" id="answer" class="answer">
+            <input type="number" name="" id="answer" class="answer">
             <input type="submit" name="answer" value="Volgende vraag">
         </form>
     </div>
