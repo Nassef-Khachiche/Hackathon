81429351c82cf7366fd34017b15a4c06267136ee
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 10:04:04 +0100

    conflict quiz.php

--- quiz_questions.php
+++ quiz_questions.php
@@ -7,16 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/quiz-style.css">
-<<<<<<< HEAD
-=======
     <script src="js/script.js" defer></script>
->>>>>>> main
     <title>Quiz questions</title>
 </head>
 
 <body>
     <?php require 'inc/nav.php'; ?>
-<<<<<<< HEAD
     <?php include 'function/functions.php'; ?>
     <div class="container-question">
         <h1 class="question-count"> Vraag 1/12 </h1>
@@ -24,7 +20,6 @@
         <form action="quiz_questions.php" method="post">
             <input type="number" name="" id="answer" class="answer">
             <input type="submit" name="answer" value="Volgende vraag">
-=======
     <div class="container-question">
         <h1 class="question-count"> Vraag 1/12 </h1>
         <?php //require 'functions/functions.php';
@@ -34,7 +29,6 @@
         <form action="quiz_questions.php" method="post">
             <input type="number" name="answer" id="answer" class="answer">
             <input type="button" value="Volgende vraag" class="btnSubmitQuestion">
->>>>>>> main
         </form>
     </div>
 </body>
