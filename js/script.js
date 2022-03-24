3cf21b5704a7ff1f28994e19aeeb5d7f38f611c5
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 16:22:27 +0100

    answers array made

--- js/script.js
+++ js/script.js
@@ -3,21 +3,23 @@ const vraag = document.querySelector('.question-count');
 const nextQuestion = document.querySelector('.dummy');
 const emptyDiv = document.querySelector('.elements');
 
-var answer = document.querySelector('.elements');
+let answer = document.querySelectorAll('answer');
 let answerList = [];
 var vragenAantal = 1;
 var index = 0;
+let answerIndex = 0;
 
-const questions = ["Ga je vaak op schoolreis?",
+const questions = [
+    "Ga je vaak op schoolreis?",
     "Doe je mee aan een sport? of een andere buiten schoolse activiteit",
     "Komen jou vrienden vaak bij jou thuis?", "Hebben je ouders een auto?",
     "Hebben jullie thuis een TV?", "Hoevaak per jaar gaan jullie uit met het gezin?",
     "Voel je je vaak buiten gesloten?", "Word je gepest?",
     "Heb je de nieuwste spelcomputer, speelgoed, schoenen etc?"
-];
+    ];
 
 const number = '<input type="number" id="answer" class="answer">'
-const radio = '<input type="radio" name="radio" id="answer" class="answer" value="Ja"><label for"radio">Ja </label> <input type="radio" name="radio" id="answer" class="answer" value="Nee"> <label for"radio">Nee </label> <input type="radio" name="radio" id="answer" class="answer" value="Soms"><label for"radio">Soms</label> <input type="radio" name="radio" id="answer" class="answer" value="Niet"> <label for"radio">Antwoord ik liever niet </label>'
+const radio = '<input type="radio" name="radio" id="answer" class="answer" value="Ja"><label class="label" for"radio">Ja </label> <input type="radio" name="radio" id="answer" class="answer" value="Nee"> <label for"radio">Nee </label> <input type="radio" name="radio" id="answer" class="answer" value="Soms"><label for"radio">Soms</label> <input type="radio" name="radio" id="answer" class="answer" value="Niet"> <label for"radio">Antwoord ik liever niet </label>'
 
 volgendeVraag.addEventListener('click', function () {
     if (vragenAantal != 10) {
@@ -59,11 +61,12 @@ volgendeVraag.addEventListener('click', function () {
         nextQuestion.textContent = questions[index];
         index++
 
-        answerList.push(answer);
+        answerList.push(answer[answerIndex]);
+        answerIndex++;
         console.log(answerList);
-    }
 
-    if (vragenAantal == 10) {
-
+        if (vragenAantal == 11) {
+            window.location = "einde.php";
+        }
     }
 })
