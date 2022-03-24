d4b3860f8554568d0df6aa98a4b5481dbbc338e0
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 10:05:13 +0100

    commit quiz.php

--- quiz_questions.php
+++ quiz_questions.php
@@ -13,13 +13,6 @@
 
 <body>
     <?php require 'inc/nav.php'; ?>
-    <?php include 'function/functions.php'; ?>
-    <div class="container-question">
-        <h1 class="question-count"> Vraag 1/12 </h1>
-        <h2 class="dummy"><?php get_question(0); ?></h2>
-        <form action="quiz_questions.php" method="post">
-            <input type="number" name="" id="answer" class="answer">
-            <input type="submit" name="answer" value="Volgende vraag">
     <div class="container-question">
         <h1 class="question-count"> Vraag 1/12 </h1>
         <?php //require 'functions/functions.php';
