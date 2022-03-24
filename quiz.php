999c0dfee58a82a551de5ee7c8351c24dfa071fe
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-24 10:04:49 +0100

    conflict quiz.php

--- quiz.php
+++ quiz.php
@@ -23,17 +23,7 @@
             <div class="input-and-btn">
                 <label for="input-name">Vul je naam in!</label>
                 <input type="text" name="input-name" class="name-input">
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-                <a href="quiz_questions.php" class="start-quiz-btn">
-                    <button class="start-quiz-btn">Start quiz</button>
-                </a>
-=======
                 <a href="quiz_questions.php" class="start-quiz-btn">Start quiz</a>
->>>>>>> Stashed changes
-=======
-                <a href="" class="start-quiz-btn">Start quiz</a>
->>>>>>> main
             </div>
             <div class="img-div">
             <img src="assets/quiz-img.png" alt="quiz logo" class="quiz-img">
