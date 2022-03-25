6dc0c95030e6745b10e68ea7b74ab8148071dc4e
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-03-25 11:13:33 +0100

    List added

--- einde.php
+++ einde.php
@@ -18,21 +18,21 @@
     <?php require 'inc/nav.php'; ?>
     <div class="end-container">
         <h1>Dankjewel voor het invullen!</h1>
-        <button class="endButton">Verstuur!</button>
         <div class="container" style="font-size: 20px;">
-        <ul style="list-style-type: none; text-align: left;">
-            <li>Hoe oud ben je: 12 Jaar</li>
-            <li>Ga je vaak mee op schoolreis: Nee</li>
-            <li>Doe je mee aan een sport? of een andere buiten schoolse activiteit: Nee</li>
-            <li>Komen jou vrienden vaak bij jou thuis?: Nee</li>
-            <li>Hebben jullie thuis een TV?: Ja</li>
-            <li>Hebben je ouders een auto?: Ja</li>
-            <li>Hoevaak per jaar gaan jullie uit met het gezin?: 3</li>
-            <li>Voel je je vaak buiten gesloten?: Nee</li>
-            <li>Word je gepest?: Ja</li>
-            <li>Heb je de nieuwste spelcomputer, speelgoed, schoenen etc?: Nee</li>
+        <ul style="list-style-type: none; text-align: center; background: orange; border-radius: 10px; padding: 15px; color: white; margin: 0px;">
+            <li>Hoe oud ben je: <strong>strong 12 Jaar</strong></li>
+            <li>Ga je vaak mee op schoolreis: <strong>Nee</strong></li>
+            <li>Doe je mee aan een sport? of een andere buiten schoolse activiteit: <strong>Nee</strong></li>
+            <li>Komen jou vrienden vaak bij jou thuis?: <strong>Nee</strong></li>
+            <li>Hebben jullie thuis een TV?: <strong>Ja</strong></li>
+            <li>Hebben je ouders een auto?: <strong>Ja</strong></li>
+            <li>Hoevaak per jaar gaan jullie uit met het gezin?: <strong>3</strong></li>
+            <li>Voel je je vaak buiten gesloten?: <strong>Nee</strong></li>
+            <li>Word je gepest?: <strong>Ja</strong></li>
+            <li>Heb je de nieuwste spelcomputer, speelgoed, schoenen etc?: <strong>Nee</strong></li>
         </ul>
     </div>
+        <button class="endButton">Verstuur!</button>
     </div>
     <?php require 'inc/footer.php'; ?>
 </body>
